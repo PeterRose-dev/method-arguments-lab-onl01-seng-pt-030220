cc0e42d10a91753b7fd4603360b33c7e3ab40f6d
Peter Rose <peterroseq1@gmail.com>
2020-03-03 06:56:23 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# lib/introduction.rb 
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, coding_lang)
+  puts "Hi, my name is #{name} and I am learning to program in #{coding_lang}."
+end
