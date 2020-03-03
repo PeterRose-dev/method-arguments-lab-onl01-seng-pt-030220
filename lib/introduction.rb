11a4be16bf564805d37c4598c74c9a903de385d0
Peter Rose <peterroseq1@gmail.com>
2020-03-03 06:55:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,4 +4,6 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language(name, )
+def introduction_with_language(name, coding_lang)
+  puts "Hi, my name is #{name} and I am leaerning to program in #{coding_lang}."
+end
